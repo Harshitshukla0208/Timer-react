a90f6f3bd7453c80b1235768d3bc7f23f440f708
Harshit Shukla <mrharshit2409@gmail.com>
2024-02-18 12:26:20 +0530

    ui completed

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './Countdown.css'
 
 function Countdown() {
+    const [target, setTarget] = useState(null)
     return (
         <>
-            <h1>Countdown Timer App</h1>
-            <div id="input">
-                <input type="datetime-local" id="date-time" />
+            <h1>Timer</h1>
+            <div class="container">
+                <div class="input-container">
+                    <input type="datetime-local" id="date-time" />
+                </div>
                 <button id="submit-btn">Start</button>
             </div>
+
+            <div id="display">
+                <ul>
+                    <li><span id="days">10</span>days</li>
+                    <li><span id="hours">2</span>hours</li>
+                    <li><span id="minutes">55</span>minutes</li>
+                    <li><span id="seconds">5</span>seconds</li>
+                </ul>
+            </div>
+
         </>
     )
 }
